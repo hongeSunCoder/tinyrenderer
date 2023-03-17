2c516c6a656464db5cafed379af0fb1f5d1b99f0
孙红杰 <sunhongjie@sunhongjiedeMac-mini.local>
2023-03-17 17:36:52 +0800

    lesson 7 - shadow buffers

--- lesson6/main.cpp
+++ lesson6/main.cpp
@@ -2,7 +2,8 @@
 #include "../lesson1/model.h"
 
 Model *model = NULL;
-Vec3f light_dir(1, 1, 1);
+float *shadowbuffer = NULL;
+Vec3f light_dir(1, 1, 0);
 Vec3f eye(1, 1, 3);
 Vec3f center(0, 0, 0);
 Vec3f up(0, 1, 0);
@@ -11,33 +12,37 @@ const int height = 800;
 
 struct GouraudShader : public IShader
 {
-    Vec3f varying_intensity;
-    mat<2, 3, float> varying_uv;
 
-    mat<4, 4, float> uniform_M;   // Projection*ModelView
-    mat<4, 4, float> uniform_MIT; // Projection*ModelView*invert_transpose()
+    mat<2, 3, float> varying_uv;
+    mat<3, 3, float> varying_tri;
+
+    mat<4, 4, float> uniform_M;       // Projection*ModelView
+    mat<4, 4, float> uniform_MIT;     // Projection*ModelView*invert_transpose()
+    mat<4, 4, float> uniform_Mshadow; //
+
+    GouraudShader(Matrix M, Matrix MIT, Matrix MS) : uniform_M(M), uniform_MIT(MIT), uniform_Mshadow(MS), varying_uv(), varying_tri() {}
 
     virtual Vec4f vertex(int iface, int nthvert)
     {
-        varying_intensity[nthvert] = std::max(0.f, model->normal(iface, nthvert) * light_dir);
+
         varying_uv.set_col(nthvert, model->uv(iface, nthvert));
-
-        Vec4f gl_Vertex = embed<4>(model->vert(iface, nthvert));
-
-        return Viewport * Projection * ModelView * gl_Vertex;
+        Vec4f gl_Vertex = Viewport * Projection * ModelView * embed<4>(model->vert(iface, nthvert));
+        varying_tri.set_col(nthvert, proj<3>(gl_Vertex / gl_Vertex[3]));
+        return gl_Vertex;
     }
 
     virtual bool fragment(Vec3f bar, TGAColor &color)
     {
 
-        // float intensity = varying_intensity * bar;
+        Vec4f sb_p = uniform_Mshadow * embed<4>(varying_tri * bar);
+        sb_p = sb_p / sb_p[3];
+
+        int idx = int(sb_p[0]) + int(sb_p[1]) * width; // index in the shadowbuffer array
+        float shadow = .3 + .7 * (shadowbuffer[idx] < sb_p[2] + 43.34);
 
         Vec2f uv = varying_uv * bar;
-
         Vec3f n = proj<3>(uniform_MIT * embed<4>(model->normal(uv))).normalize();
-
         Vec3f l = proj<3>(uniform_M * embed<4>(light_dir)).normalize();
-
         // reflected light
         Vec3f r = (n * (n * l * 2.f) - l).normalize();
 
@@ -47,7 +52,30 @@ struct GouraudShader : public IShader
 
         color = model->diffuse(uv);
         for (int i = 0; i < 3; i++)
-            color[i] = std::min<float>(5 + color[i] * (1 * diff + .0 * spec), 255);
+            color[i] = std::min<float>(5 + color[i] * shadow * (1.2 * diff + .6 * spec), 255);
+        return false;
+    }
+};
+
+struct DepthShader : public IShader
+{
+    mat<3, 3, float> varying_tri;
+
+    DepthShader() : varying_tri() {}
+
+    virtual Vec4f vertex(int iface, int nthvert)
+    {
+        Vec4f gl_Vertex = embed<4>(model->vert(iface, nthvert));
+        gl_Vertex = Viewport * Projection * ModelView * gl_Vertex;
+
+        varying_tri.set_col(nthvert, proj<3>(gl_Vertex / gl_Vertex[3]));
+        return gl_Vertex;
+    }
+
+    virtual bool fragment(Vec3f bar, TGAColor &color)
+    {
+        Vec3f p = varying_tri * bar;
+        color = TGAColor(255, 255, 255) * (p.z / depth);
         return false;
     }
 };
@@ -63,33 +91,59 @@ int main(int argc, char **argv)
     {
         model = new Model("../obj/african_head/african_head.obj");
     }
-
-    lookat(eye, center, up);
-    viewport(width / 8, height / 8, width * 3 / 4, height * 3 / 4);
-    projection(-1.f / (eye - center).norm());
-    light_dir.normalize();
-
-    TGAImage image(width, height, TGAImage::RGB);
-    TGAImage zbuffer(width, height, TGAImage::GRAYSCALE);
-
-    GouraudShader shader;
-    shader.uniform_M = Projection * ModelView;
-    shader.uniform_MIT = (Projection * ModelView).invert_transpose();
-    for (int i = 0; i < model->nfaces(); i++)
+    float *zbuffer = new float[width * height];
+    shadowbuffer = new float[width * height];
+    for (int i = width * height; --i;)
     {
-        Vec4f screen_coords[3];
-        for (int j = 0; j < 3; j++)
-        {
-            screen_coords[j] = shader.vertex(i, j);
-        }
-        triangle(screen_coords, shader, image, zbuffer);
+        zbuffer[i] = shadowbuffer[i] = -std::numeric_limits<float>::max();
     }
 
-    // image.flip_vertically(); // to place the origin in the bottom left corner of the image
-    // zbuffer.flip_vertically();
-    image.write_tga_file("output.tga");
-    zbuffer.write_tga_file("zbuffer.tga");
+    light_dir.normalize();
+
+    {
+        TGAImage depth(width, height, TGAImage::RGB);
+        lookat(light_dir, center, up);
+        viewport(width / 8, height / 8, width * 3 / 4, height * 3 / 4);
+        projection(0);
+
+        DepthShader depthShader;
+        Vec4f screen_coords[3];
+        for (int i = 0; i < model->nfaces(); i++)
+        {
+            for (int j = 0; j < 3; j++)
+            {
+                screen_coords[j] = depthShader.vertex(i, j);
+            }
+
+            triangle(screen_coords, depthShader, depth, shadowbuffer);
+        }
+
+        depth.write_tga_file("depth.tga");
+    }
+    Matrix M = Viewport * Projection * ModelView;
+    {
+
+        lookat(eye, center, up);
+        viewport(width / 8, height / 8, width * 3 / 4, height * 3 / 4);
+        projection(-1.f / (eye - center).norm());
+        TGAImage image(width, height, TGAImage::RGB);
+
+        GouraudShader shader(ModelView, (Projection * ModelView).invert_transpose(), M * (Viewport * Projection * ModelView).invert());
+        Vec4f screen_coords[3];
+        for (int i = 0; i < model->nfaces(); i++)
+        {
+            for (int j = 0; j < 3; j++)
+            {
+                screen_coords[j] = shader.vertex(i, j);
+            }
+            triangle(screen_coords, shader, image, zbuffer);
+        }
+
+        image.write_tga_file("output.tga");
+    }
 
     delete model;
+    delete[] zbuffer;
+    delete[] shadowbuffer;
     return 0;
 }
